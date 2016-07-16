1e34d055afc4d60dd0b6e90e4d6781e246b83765
Jorge Luiz <JorgeLuiz@Jorges-MacBook-Pro.local>
2016-07-16 19:57:10 -0300

    first commit

---
+++ Pods/Target Support Files/Pods-devlsops-firebase/Pods-devlsops-firebase-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_devlsops_firebaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_devlsops_firebaseVersionString[];
+
---
+++ Pods/Target Support Files/Pods-devlsops-firebaseTests/Pods-devlsops-firebaseTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_devlsops_firebaseTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_devlsops_firebaseTestsVersionString[];
+
